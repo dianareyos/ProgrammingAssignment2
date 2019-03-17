646241539e6113e7c5d8d617190cff55986e6056
dianareyos <47272303+dianareyos@users.noreply.github.com>
2019-03-18 00:34:17 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Course : Coursera - R Programming
+## Programming Assignment 2
+## Author: Nitin Kaul
+### Function Descriptions ###
+## makeCacheMatrix - function creates a matrix and sets inverse calculation in cache
+## cacheSolve - function computes inverse of matrix
+### Example Usage ###
+# source("cachematrix.R")   #Source the file
+# mat1 <- makeCacheMatrix(matrix(1:4, 2))   #set a matrix   
+# class(matrix1)    #check and confirm the class of mat1 as list
+# mat1$get()    #get the created matrix details
+# mat1$getInverse()   #checked the Iverse vaule is NULL
+# mat1$set(matrix(5:8,2))   #set a new matrix using set function
+# mat1$get()    #check if new matrix was set through set function
+# cacheSolve(mat1)    #get the inverse of matrix and set the cache
+# cacheSolve(mat1)    #check the inverse of matrix and inverse is being used from cache
+# mat1$getInverse()   #check the inverset has been set in cache
 
-## Write a short comment describing this function
+
+## function makeCacheMatrix creates a special matrix object and can cache its inverse
+## and returns a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
   # assigning inverseMatrix as NULL value
   inverseMatrix <- NULL
   # get function to get Matrix
